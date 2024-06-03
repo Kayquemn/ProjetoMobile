ceb95b72d266186a857e44b45a3e2dbb47c96d15
Kayquemn <145171658+Kayquemn@users.noreply.github.com>
2024-06-03 19:49:49 -0300

    Update Botao.js

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, Alert } from "react-native";
 
-import Texto from './Texto'
+import Texto from './Texto';
 
-export default function Botao({textoBotao}){
-return  <TouchableOpacity style={styles.botao}>
-<Texto style={styles.botaoTexto}>{textoBotao}</Texto>
-</TouchableOpacity>
+export default function Botao({ textoBotao }) {
+    const [mensagem, setMensagem] = useState('');
+
+    const handlePress = () => {
+        setMensagem();
+        // Mostrar a mensagem de "Adicionado"
+        Alert.alert('MUITO BOM GEARHEAD', 'Adicionado a lista de desejos!');
+    };
+
+    return (
+        <>
+            <TouchableOpacity style={styles.botao} onPress={handlePress}>
+                <Texto style={styles.botaoTexto}>{textoBotao}</Texto>
+            </TouchableOpacity>
+            <Texto>{mensagem}</Texto>
+        </>
+    );
 }
 
-
 const styles = StyleSheet.create({
-botao: {
-     marginTop: 16,
-    backgroundColor: 'blue',
-    paddingVertical: 20,
-    borderRadius: 15,
-   
-  
-
+    botao: {
+        marginTop: 16,
+        backgroundColor: '#FF8F00',
+        paddingVertical: 20,
+        borderRadius: 15,
+        width: "80%",
+        marginLeft: 43,
+    },
+    botaoTexto: {
+        textAlign: "center",
+        color: "white",
+        fontSize: 20,
+        lineHeight: 26,
+        fontWeight: "bold",
     },
 });
