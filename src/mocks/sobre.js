8916016c78a846e7c313111c388d78a69a96f673
Kayquemn <145171658+Kayquemn@users.noreply.github.com>
2024-06-03 19:54:03 -0300

    Update sobre.js

--- src/mocks/sobre.js
+++ src/mocks/sobre.js
@@ -6,7 +6,9 @@ const sobre ={
     info: {
       logo: Logo,
       Titulosobre: "Quem Somos ",
-      efeito: "Na AutoParts, estamos comprometidos em oferecer a você as melhores soluções para a preparação do seu carro. Como entusiastas automotivos, entendemos a importância de ter as peças certas para maximizar o desempenho do seu veículo. Nossa vasta seleção de peças abrange desde sistemas de exaustão de alto desempenho até filtros de ar especializados, passando por suspensões ajustáveis e muito mais. Trabalhamos apenas com os principais fabricantes do setor, garantindo que cada peça que você adquire conosco seja de qualidade excepcional e projetada para elevar o potencial do seu carro ao próximo nível.",
+      efeito: "Na AutoParts, estamos comprometidos em oferecer a você as melhores soluções para a preparação do seu carro. ",
+      efeito1:"Como entusiastas automotivos, entendemos a importância de ter as peças certas para maximizar o desempenho do seu veículo.",
+      efeito2: "Nossa vasta seleção de peças abrange desde sistemas de exaustão de alto desempenho até filtros de ar especializados, passando por suspensões ajustáveis e muito mais. Trabalhamos apenas com os principais fabricantes do setor, garantindo que cada peça que você adquire conosco seja de qualidade excepcional e projetada para elevar o potencial do seu carro ao próximo nível."
     },
    
 }
